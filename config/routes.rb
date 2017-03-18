e76616b1e2a6cdeb4876fa124c6f42e11f8d4f01
rombod madgidi <rambodmagid@yahoo.com>
2017-03-18 16:11:19 -0600

    komsrd

--- config/routes.rb
+++ config/routes.rb
@@ -3,17 +3,33 @@ Rails.application.routes.draw do
 
 
 
-  resources :orders, only: [:index, :show, :create, :destroy]
-  resources :products
+  
+resources :orders, only: [:index, :show, :create, :destroy]
+resources :products  
 
-post 'static_pages/thank_you'
 
+  root 'static_pages#index'
 
-  root :to => 'static_pages#index'
-get '/index' => 'static_pages#index'
-get '/about' => 'static_pages#about'
-get '/contact' => 'static_pages#contact'
-get '/landing_page' => 'static_pages#landing_page'
+  get  '/index', to: 'static_pages#index'
+  
+  get  '/about', to: 'static_pages#about'
+
+  get  '/contact', to: 'static_pages#contact'
+
+  get 'landing_page', to: 'static_pages#landing_page'
+
+  post 'static_pages/thank_you'
+
+  
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
+
+
+
+
+
+
+
+
+  
