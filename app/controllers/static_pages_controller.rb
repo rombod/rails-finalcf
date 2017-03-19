a71ac207d6d4891597b29be631371fa9959a993f
rombod madgidi <rambodmagid@yahoo.com>
2017-03-19 12:25:29 -0600

    djrmhg

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -5,5 +5,15 @@ class StaticPagesController < ApplicationController
   def landing_page
   @products = Product.limit(3)
 end
+
+def thank_you
+    @name = params[:name]
+    @email = params[:email]
+    @message = params[:message]
+    ActionMailer::Base.mail(:from => @email,
+        :to => 'mahmood.madgidi@farauto.ca',
+        :subject => "A new contact form message from #{@name}",
+        :body => @message).deliver_now
+  end
   
 end
