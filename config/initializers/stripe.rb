5597d04ae11a419308585d95a76d0ff3444da8e0
rombod <rambodmagid@yahoo.com>
2017-05-02 09:33:15 -0600

    6

--- config/initializers/stripe.rb
+++ config/initializers/stripe.rb
@@ -3,11 +3,11 @@ if Rails.env.production?
     :publishable_key => ENV['STRIPE_PUBLISHABLE_KEY'],
     :secret_key => ENV['STRIPE_SECRET_KEY']
   }
-else
+  else
   Rails.configuration.stripe = {
     :publishable_key => 'pk_test_oEPFMyKxVpetNnAknIEspL4V',
-    :secret_key => 'sk_test_biL0NmoyG4a7Fr4HVmnCb2NY '
+    :secret_key => 'sk_test_biL0NmoyG4a7Fr4HVmnCb2NY'
   }
 end
 
-Stripe.api_key = Rails.configuration.stripe[:secret_key]
+    Stripe.api_key = Rails.configuration.stripe[:secret_key]
